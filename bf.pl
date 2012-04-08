1cf1d408346fde3f9183c3d8742ab322f0f59040
Ondrej Kupka <ondra.cap@gmail.com>
2012-04-08 21:05:45 +0200

    Finding cost of the path finished

--- bf.pl
+++ bf.pl
@@ -4,57 +4,98 @@
 
 %% Graph supplementary rules
 
-% g_vertices(+Gid, -Vertices)
+:-op(400, xfy, ->).
+:-op(500, xfx, /).
+
 g_vertices(Gid, Vertices) :-
 	g(Gid, Vertices, _).
 
-% g_vertex(+Gid, ?Vertex)
+g_edges(Gid, Edges) :-
+	g(Gid, _, Edges).
+
 g_vertex(Gid, Vertex) :-
-	member(Vertex, g_vertices(Gid)).
+	g_vertices(Gid, Vertices),
+	member(Vertex, Vertices).
 
 %% Bellman-Ford
 
-bf_set_shortest(Gid, From, To, Cost) :-
-	bf_shortest(Gid, _, To, _),
-	retract(bf_shortest(Gid, _, To, _)),
-	asserta(bf_shortest(Gid, From, To, Cost)),
+bf_set_shortest(Gid, From->To/Cost) :-
+	retract(bf_shortest(Gid, From->To/_)),
+	asserta(bf_shortest(Gid, From->To/Cost)),
 	!.
-bf_set_shortest(Gid, From, To, Cost) :-
-	asserta(bf_shortest(Gid, From, To, Cost)).
+bf_set_shortest(Gid, From->To/Cost) :-
+	asserta(bf_shortest(Gid, From->To/Cost)).
 
-% bf_get_shortest(+Gid, ?From, ?To, ?Cost)
-bf_get_shortest(Gid, From, To, Cost) :-
-	bf_shortest(Gid, From, To, Cost).
+bf_visited(Gid, Vertex) :-
+	bf_shortest(Gid, _->Vertex/_).
 
-% bf(+Gid, +Start, +Stop, -Cost, -Path)
 bf(Gid, Start, Stop, Cost, Path) :-
 	g_vertex(Gid, Start),
 	g_vertex(Gid, Stop),
-	bf_set_shortest(Gid, nill, Start, 0),
-	bf_loop(Gid, length(g_vertices(Gid))),
-	bf_assemble_path(Start, Stop, Cost, Path).
+	bf_set_shortest(Gid, nill->Start/0),
+	g_edges(Gid, Edges),
+	length(Edges, M),
+	bf_loop(Gid, M),
+	bf_assemble_path(Gid, Start, Stop, Cost, Path),
+	!.
 
+bf_loop(_, 0) :- !.
 bf_loop(Gid, M) :-
-	bf_loop(Gid, M, true).
-
-bf_loop(Gid, 0, _) :- !.
-bf_loop(Gid, _, false) :- !.
-bf_loop(Gid, M, _) :-
 	Mnext is M - 1,
-	bf_explore(Gid, Modified),
-	bf_loop(Gid, Mnext, Modified).
+	bf_explore(Gid),
+	bf_loop(Gid, Mnext).
 
-bf_explore(Gid, Modified) :-
-	vertices(Gid, Vertices),
-	bf_explore(Gid, Vertices, false, Modified).
+bf_explore(Gid) :-
+	g_vertices(Gid, Vertices),
+	bf_explore(Gid, Vertices).
 
-bf_explore(_, [], Modified, Modified).
-bf_explore(Gid, [V|Rest], false, ModifiedReturn) :-
-	bf_update(Gid, V, Modified),
-	bf_explore(Gid, Rest, Modified, ModifiedReturn).
-bf_explore(Gid, [V|Rest], true, ModifiedReturn) :-
-	bf_update(Gid, V, _),
-	bf_explore(Gid, Rest, true, ModifiedReturn).
+bf_explore(_, []) :- !.
+bf_explore(Gid, [Vertex|Rest]) :-
+	bf_visited(Gid, Vertex),
+	bf_update(Gid, Vertex),
+	bf_explore(Gid, Rest),
+	!.
+bf_explore(Gid, [_|Rest]) :-
+	bf_explore(Gid, Rest).
 
-bf_update(Gid, Vertex, Modified) :-
+bf_update(Gid, Vertex) :-
+	findall(Vertex->To/Cost,
+	(
+		g_edges(Gid, E), 
+		member(Vertex->To/Cost, E)
+	), Adjacent),
+	bf_update_path(Gid, Vertex, Adjacent).
 
+bf_update_path(_, _, []) :- !.
+bf_update_path(Gid, From, [From->To/Cost|Rest]) :-
+	bf_shortest(Gid, _->To/TCost),
+	bf_shortest(Gid, _->From/FCost),
+	PCost is FCost + Cost,
+	bf_update_record(Gid, From->To, TCost, PCost),
+	bf_update_path(Gid, From, Rest),
+	!.
+bf_update_path(Gid, From, [From->To/Cost|Rest]) :-
+	bf_shortest(Gid, _->From/FCost),
+	PCost is FCost + Cost,
+	bf_set_shortest(Gid, From->To/PCost),
+	bf_update_path(Gid, From, Rest).
+
+bf_update_record(_, _, OldCost, NewCost) :-
+	OldCost =< NewCost,
+	!.
+bf_update_record(Gid, From->To, OldCost, NewCost) :-
+	NewCost < OldCost,
+	bf_set_shortest(Gid, From->To/NewCost).
+
+bf_assemble_path(Gid, _, _, _, Path) :-
+	findall(U->V/C, (
+		bf_shortest(Gid, U->V/C)
+	), Path).
+
+%% Testing data
+
+g(
+	1,
+	[a,b,c,d,e,f],
+	[a->b/1, b->c/1, c->d/1, d->e/1, e->f/1, a->d/1]
+).
